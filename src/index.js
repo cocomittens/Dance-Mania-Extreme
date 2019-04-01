8ffc9bfb2749ec2e84b83dda24fe3be3f413173f
ioqren <corriestoddard@gmail.com>
2019-03-31 17:42:51 -0700

    Add menu screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-let upPressed = false;
-let downPressed = false;
-let rightPressed = false;
-let leftPressed = false;
+import { handleInput, keyDownHandler, keyUpHandler, drawArrows } from './game.js';
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+document.querySelectorAll('.button')[0].addEventListener('click', function (event) {
+    let menu = document.getElementsByClassName('menuContainer')[0];
+    let canvas = document.getElementsByClassName('canvasContainer')[0];
+    menu.style.display = 'none';
+    canvas.style.display = 'block';
+    drawArrows();
+});
 
 const body = document.getElementsByTagName('body')[0];
 let id;
@@ -13,95 +15,8 @@ function gameLoop() {
     // Animate stuff
     id = requestAnimationFrame(gameLoop)
 }
-
-body.addEventListener('click', () => {
-    cancelAnimationFrame(id)
-})
-
-
-function handleInput(key, action) {
-    const canvas = document.getElementById('canvas');
-    const c = canvas.getContext('2d');
-
-    let right_arrow = new Image();
-    let left_arrow = new Image();
-    let up_arrow = new Image();
-    let down_arrow = new Image();
-
-    let right_arrow_active = new Image();
-    let left_arrow_active = new Image();
-    let up_arrow_active = new Image();
-    let down_arrow_active = new Image();
-
-    right_arrow.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_right.png";
-    left_arrow.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_left.png";
-    up_arrow.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_up.png";
-    down_arrow.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_down.png";
-
-    right_arrow_active.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_right_active.png";
-    left_arrow_active.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_left_active.png";
-    up_arrow_active.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_up_active.png";
-    down_arrow_active.src = "/Users/cgripens/Desktop/AppAcademy/dmr/Sprites/arrow_down_active.png";
-
-    if(action === "down") {
-        switch(key){
-            case "ArrowUp":
-                c.drawImage(up_arrow_active, 200, 0);
-                break;
-            case "ArrowDown":
-                c.drawImage(down_arrow_active, 400, 0);
-                break;
-            case "ArrowRight":
-                c.drawImage(right_arrow_active, 100, 0);
-                break;
-            case "ArrowLeft":
-                c.drawImage(left_arrow_active, 300, 0);
-                break;
-        }
-    } else {
-        switch (key) {
-            case "ArrowUp":
-                c.drawImage(up_arrow, 200, 0);
-                break;
-            case "ArrowDown":
-                c.drawImage(down_arrow, 400, 0);
-                break;
-            case "ArrowRight":
-                c.drawImage(right_arrow, 100, 0);
-                break;
-            case "ArrowLeft":
-                c.drawImage(left_arrow, 300, 0);
-                break;
-        }
-    }
-}
-
-function keyDownHandler(e) {
-    switch(e.key){
-        case "ArrowUp":
-            upPressed = true;
-        case "ArrowDown":
-            downPressed = true;
-        case "ArrowRight":
-            rightPressed = true;
-        case "ArrowLeft":
-            leftPressed = true;
-    }
-
-    handleInput(e.key, "down")
-}
-
-function keyUpHandler(e) {
-    switch (e.key) {
-        case "ArrowUp":
-            upPressed = false;
-        case "ArrowDown":
-            downPressed = false;
-        case "ArrowRight":
-            rightPressed = false;
-        case "ArrowLeft":
-            leftPressed = false;
-    }
-
-    handleInput(e.key, "up")
+if(body) {
+    body.addEventListener('click', () => {
+        cancelAnimationFrame(id)
+    })
 }
