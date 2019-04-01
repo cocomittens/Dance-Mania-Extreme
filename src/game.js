f5a175a8ef628da8c8e5f07058d95e928edf50ec
ioqren <corriestoddard@gmail.com>
2019-03-31 18:22:09 -0700

    Update animation script

--- src/game.js
+++ src/game.js
@@ -1,123 +1,89 @@
-let upPressed = false;
-let downPressed = false;
-let rightPressed = false;
-let leftPressed = false;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
-export function drawArrows() {
-    const canvas = document.getElementById('canvas');
-    const c = canvas.getContext('2d');
 
-    let right_arrow = new Image();
-    let left_arrow = new Image();
-    let up_arrow = new Image();
-    let down_arrow = new Image();
+// export function handleInput(key, action) {
+//     const canvas = document.getElementById('canvas');
+//     const c = canvas.getContext('2d');
 
-    right_arrow.src = "./sprites/arrow_right.png";
-    left_arrow.src = "./sprites/arrow_left.png";
-    up_arrow.src = "./sprites/arrow_up.png";
-    down_arrow.src = "./sprites/arrow_down.png";
+//     let right_arrow = new Image();
+//     let left_arrow = new Image();
+//     let up_arrow = new Image();
+//     let down_arrow = new Image();
 
-    right_arrow.onload = function () {
-        c.drawImage(right_arrow, 100, 0);
-    }
-    up_arrow.onload = function () {
-        c.drawImage(up_arrow, 200, 0);
-    }
-    left_arrow.onload = function () {
-        c.drawImage(left_arrow, 300, 0);
-    }
-    down_arrow.onload = function () {
-        c.drawImage(down_arrow, 400, 0);
-    }
+//     let right_arrow_active = new Image();
+//     let left_arrow_active = new Image();
+//     let up_arrow_active = new Image();
+//     let down_arrow_active = new Image();
 
-}
+//     right_arrow.src = "./sprites/arrow_right.png";
+//     left_arrow.src = "./sprites/arrow_left.png";
+//     up_arrow.src = "./sprites/arrow_up.png";
+//     down_arrow.src = "./sprites/arrow_down.png";
 
-export function handleInput(key, action) {
-    const canvas = document.getElementById('canvas');
-    const c = canvas.getContext('2d');
+//     right_arrow_active.src = "./sprites/arrow_right_active.png";
+//     left_arrow_active.src = "./sprites/arrow_left_active.png";
+//     up_arrow_active.src = "./sprites/arrow_up_active.png";
+//     down_arrow_active.src = "./sprites/arrow_down_active.png";
 
-    let right_arrow = new Image();
-    let left_arrow = new Image();
-    let up_arrow = new Image();
-    let down_arrow = new Image();
+//     if (action === "down") {
+//         switch (key) {
+//             case "ArrowUp":
+//                 c.drawImage(up_arrow_active, 200, 0);
+//                 break;
+//             case "ArrowDown":
+//                 c.drawImage(down_arrow_active, 400, 0);
+//                 break;
+//             case "ArrowRight":
+//                 c.drawImage(right_arrow_active, 100, 0);
+//                 break;
+//             case "ArrowLeft":
+//                 c.drawImage(left_arrow_active, 300, 0);
+//                 break;
+//         }
+//     } else {
+//         switch (key) {
+//             case "ArrowUp":
+//                 c.drawImage(up_arrow, 200, 0);
+//                 break;
+//             case "ArrowDown":
+//                 c.drawImage(down_arrow, 400, 0);
+//                 break;
+//             case "ArrowRight":
+//                 c.drawImage(right_arrow, 100, 0);
+//                 break;
+//             case "ArrowLeft":
+//                 c.drawImage(left_arrow, 300, 0);
+//                 break;
+//         }
+//     }
+// }
 
-    let right_arrow_active = new Image();
-    let left_arrow_active = new Image();
-    let up_arrow_active = new Image();
-    let down_arrow_active = new Image();
+// export function keyDownHandler(e) {
+//     switch (e.key) {
+//         case "ArrowUp":
+//             upPressed = true;
+//         case "ArrowDown":
+//             downPressed = true;
+//         case "ArrowRight":
+//             rightPressed = true;
+//         case "ArrowLeft":
+//             leftPressed = true;
+//     }
 
-    right_arrow.src = "./sprites/arrow_right.png";
-    left_arrow.src = "./sprites/arrow_left.png";
-    up_arrow.src = "./sprites/arrow_up.png";
-    down_arrow.src = "./sprites/arrow_down.png";
+//     handleInput(e.key, "down")
+// }
 
-    right_arrow_active.src = "./sprites/arrow_right_active.png";
-    left_arrow_active.src = "./sprites/arrow_left_active.png";
-    up_arrow_active.src = "./sprites/arrow_up_active.png";
-    down_arrow_active.src = "./sprites/arrow_down_active.png";
+// export function keyUpHandler(e) {
+//     switch (e.key) {
+//         case "ArrowUp":
+//             upPressed = false;
+//         case "ArrowDown":
+//             downPressed = false;
+//         case "ArrowRight":
+//             rightPressed = false;
+//         case "ArrowLeft":
+//             leftPressed = false;
+//     }
 
-    if (action === "down") {
-        switch (key) {
-            case "ArrowUp":
-                c.drawImage(up_arrow_active, 200, 0);
-                break;
-            case "ArrowDown":
-                c.drawImage(down_arrow_active, 400, 0);
-                break;
-            case "ArrowRight":
-                c.drawImage(right_arrow_active, 100, 0);
-                break;
-            case "ArrowLeft":
-                c.drawImage(left_arrow_active, 300, 0);
-                break;
-        }
-    } else {
-        switch (key) {
-            case "ArrowUp":
-                c.drawImage(up_arrow, 200, 0);
-                break;
-            case "ArrowDown":
-                c.drawImage(down_arrow, 400, 0);
-                break;
-            case "ArrowRight":
-                c.drawImage(right_arrow, 100, 0);
-                break;
-            case "ArrowLeft":
-                c.drawImage(left_arrow, 300, 0);
-                break;
-        }
-    }
-}
-
-export function keyDownHandler(e) {
-    switch (e.key) {
-        case "ArrowUp":
-            upPressed = true;
-        case "ArrowDown":
-            downPressed = true;
-        case "ArrowRight":
-            rightPressed = true;
-        case "ArrowLeft":
-            leftPressed = true;
-    }
-
-    handleInput(e.key, "down")
-}
-
-export function keyUpHandler(e) {
-    switch (e.key) {
-        case "ArrowUp":
-            upPressed = false;
-        case "ArrowDown":
-            downPressed = false;
-        case "ArrowRight":
-            rightPressed = false;
-        case "ArrowLeft":
-            leftPressed = false;
-    }
-
-    handleInput(e.key, "up")
-}
+//     handleInput(e.key, "up")
+// }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import { handleInput, keyDownHandler, keyUpHandler, drawArrows } from './game.js';
 
 document.querySelectorAll('.button')[0].addEventListener('click', function (event) {
     let menu = document.getElementsByClassName('menuContainer')[0];
     let canvas = document.getElementsByClassName('canvasContainer')[0];
     menu.style.display = 'none';
     canvas.style.display = 'block';
-    drawArrows();
 });
 
 const body = document.getElementsByTagName('body')[0];
@@ -19,4 +17,171 @@ if(body) {
     body.addEventListener('click', () => {
         cancelAnimationFrame(id)
     })
+}
+
+
+var img = new Image();
+
+let right_arrow = new Image();
+let left_arrow = new Image();
+let up_arrow = new Image();
+let down_arrow = new Image();
+
+right_arrow.src = "./sprites/arrow_right.png";
+left_arrow.src = "./sprites/arrow_left.png";
+up_arrow.src = "./sprites/arrow_up.png";
+down_arrow.src = "./sprites/arrow_down.png";
+
+let right_arrow_active = new Image();
+let left_arrow_active = new Image();
+let up_arrow_active = new Image();
+let down_arrow_active = new Image();
+
+right_arrow_active.src = "./sprites/arrow_right_active.png";
+left_arrow_active.src = "./sprites/arrow_left_active.png";
+up_arrow_active.src = "./sprites/arrow_up_active.png";
+down_arrow_active.src = "./sprites/arrow_down_active.png";
+
+// User Variables - customize these to change the image being scrolled, its
+// direction, and the speed.
+
+img.src = './sprites/arrow_down_note.png';
+var CanvasXSize = 600;
+var CanvasYSize = 600;
+var speed = 2; // lower is faster
+var scale = 1.05;
+var y = 600; // vertical offset
+
+// Main program
+
+var dx = 0.75;
+var imgW;
+var imgH;
+var x = 0;
+var clearX;
+var clearY;
+var ctx;
+
+let upPressed = false;
+let downPressed = false;
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+img.onload = function () {
+    imgW = img.width * scale;
+    imgH = img.height * scale;
+
+    if (imgW > CanvasXSize) {
+        // image larger than canvas
+        x = CanvasXSize - imgW;
+    }
+    if (imgW > CanvasXSize) {
+        // image width larger than canvas
+        clearX = imgW;
+    } else {
+        clearX = CanvasXSize;
+    }
+
+    if (imgH > CanvasYSize) {
+        // image height larger than canvas
+        clearY = imgH;
+    } else {
+        clearY = CanvasYSize;
+    }
+
+    // get canvas context
+    ctx = document.getElementById('canvas').getContext('2d');
+
+    // set refresh rate
+    return setInterval(draw, speed);
+}
+
+
+function keyDownHandler(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            upPressed = true;
+            break;
+        case "ArrowDown":
+            downPressed = true;
+            break;
+        case "ArrowRight":
+            rightPressed = true;
+            break;
+        case "ArrowLeft":
+            leftPressed = true;
+            break;
+    }
+}
+
+function keyUpHandler(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            upPressed = false;
+            break;
+        case "ArrowDown":
+            downPressed = false;
+            break;
+        case "ArrowRight":
+            rightPressed = false;
+            break;
+        case "ArrowLeft":
+            leftPressed = false;
+            break;
+    }
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, clearX, clearY); // clear the canvas
+    
+    // reset, start from beginning
+    if (x > CanvasXSize) {
+        x = -imgW + x;
+    }
+    // draw additional image1
+    if (x > 0) {
+        ctx.drawImage(img, -imgW + x, y, imgW, imgH);
+    }
+    // draw additional image2
+    if (x - imgW > 0) {
+        ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
+    }
+    
+    // draw image
+    ctx.drawImage(img, x, y, imgW, imgH);
+
+    if (rightPressed) {
+        ctx.drawImage(right_arrow_active, 100, 0);
+
+    } else {
+        ctx.drawImage(right_arrow, 100, 0);
+    }
+    if (upPressed) {
+        ctx.drawImage(up_arrow_active, 200, 0);
+
+    } else {
+        ctx.drawImage(up_arrow, 200, 0);
+    }
+    if (leftPressed) {
+        ctx.drawImage(left_arrow_active, 300, 0);
+
+    } else {
+        ctx.drawImage(left_arrow, 300, 0);
+    }
+
+    if (downPressed) {
+        ctx.drawImage(down_arrow_active, 400, 0);
+
+    } else {
+        ctx.drawImage(down_arrow, 400, 0);
+    }
+
+
+
+    // amount to move
+    y -= dx;
 }
